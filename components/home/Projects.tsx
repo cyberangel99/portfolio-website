a79d8148b231e1384980b125c1ea66ba595d180f
Mary A Green <cyberangel9911@gmail.com>
2022-04-08 09:00:28 -0500

    updated projects
    added images to projects section

--- components/home/Projects.tsx
+++ components/home/Projects.tsx
@@ -1,4 +1,4 @@
-import { Flex, SimpleGrid } from '@chakra-ui/react'
+import { Flex, SimpleGrid, Box, Image } from '@chakra-ui/react'
 
 import React from 'react'
 
@@ -11,11 +11,24 @@ import WhileInView from '../shared/WhileInView'
 export default function Projects() {
   return (
     <Element name={HASH_ROUTES.PROJECTS}>
-      <Flex h={{ sm: 'full', md: '100vh' }} justifyContent="center" alignItems="flex-start" p={10}>
+      <Flex h={{ sm: 'full', md: '100vh' }} justifyContent="center" alignItems="center" p={10}>
         <WhileInView>
-          <SectionTitle title="Projects" />
+          <Box textAlign="center">
+            <SectionTitle title="Projects" />
+          </Box>
           <SimpleGrid columns={{ base: 1, lg: 3 }} spacing={0}>
-            asasas
+            <Box>
+              <Image src="../images/portfolio-1.png" alt="portfolio-1" />
+            </Box>
+            <Box>
+              <Image src="../images/portfolio-2.png" alt="portfolio-2" />
+            </Box>
+            <Box>
+              <Image src="../images/portfolio-3.png" alt="portfolio-3" />
+            </Box>
+            <Box>
+              <Image src="../images/portfolio-4.png" alt="portfolio-4" />
+            </Box>
           </SimpleGrid>
         </WhileInView>
       </Flex>
