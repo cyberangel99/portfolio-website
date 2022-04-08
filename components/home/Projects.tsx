0f6b215f94d38e0aab8cdf1f19b14121ba5cea8e
Mary A Green <cyberangel9911@gmail.com>
2022-04-07 21:46:44 -0500

    updated Projects
    updated projects section.

--- components/home/Projects.tsx
+++ components/home/Projects.tsx
@@ -1,5 +1,24 @@
+import { Flex, SimpleGrid } from '@chakra-ui/react'
+
 import React from 'react'
 
+import { Element } from 'react-scroll'
+
+import { HASH_ROUTES } from '../../constants'
+import SectionTitle from '../shared/SectionTitle'
+import WhileInView from '../shared/WhileInView'
+
 export default function Projects() {
-  return <div>Projects</div>
+  return (
+    <Element name={HASH_ROUTES.PROJECTS}>
+      <Flex h={{ sm: 'full', md: '100vh' }} justifyContent="center" alignItems="flex-start" p={10}>
+        <WhileInView>
+          <SectionTitle title="Projects" />
+          <SimpleGrid columns={{ base: 1, lg: 3 }} spacing={0}>
+            asasas
+          </SimpleGrid>
+        </WhileInView>
+      </Flex>
+    </Element>
+  )
 }
