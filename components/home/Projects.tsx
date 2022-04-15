8dd2314c72435d47e42cd7af5b03a676e2cce469
Mary A Green <cyberangel9911@gmail.com>
2022-04-14 22:57:00 -0500

    updated Projects
    added links to webpages on projects section.

--- components/home/Projects.tsx
+++ components/home/Projects.tsx
@@ -1,4 +1,4 @@
-import { Flex, SimpleGrid, Box, Image } from '@chakra-ui/react'
+import { Flex, SimpleGrid, Box, Image, Heading, Text, Link } from '@chakra-ui/react'
 
 import React from 'react'
 
@@ -11,23 +11,55 @@ import WhileInView from '../shared/WhileInView'
 export default function Projects() {
   return (
     <Element name={HASH_ROUTES.PROJECTS}>
-      <Flex h={{ sm: 'full', md: '100vh' }} justifyContent="center" alignItems="center" p={10}>
+      <Flex h={{ sm: 'full', md: 'full' }} justifyContent="center" alignItems="center" p={10}>
         <WhileInView>
           <Box textAlign="center">
             <SectionTitle title="Projects" />
           </Box>
           <SimpleGrid columns={{ base: 1, lg: 3 }} spacing={0}>
-            <Box>
-              <Image src="../images/portfolio-1.png" alt="portfolio-1" />
+            <Box ml={5}>
+              <Link href="https://cyberangel99.github.io/Farmermarket/" isExternal>
+                <Image src="../images/portfolio-1.png" alt="portfolio-1" />
+              </Link>
+              <Box color="gray.500" fontSize="sm" textAlign="center">
+                <Heading as="h4" size="md" color="gray.500" fontWeight="semibold" m={3}>
+                  Farmer`s Market
+                </Heading>
+                <Text mb={5}>Built with HTML, CSS, Javascript and UIkit framework.</Text>
+              </Box>
             </Box>
-            <Box>
-              <Image src="../images/portfolio-2.png" alt="portfolio-2" />
+            <Box ml={5}>
+              <Link href="https://cyberangel99.github.io/nextgeneration/" isExternal>
+                <Image src="../images/portfolio-2.png" alt="portfolio-2" />
+              </Link>
+              <Box color="gray.500" fontSize="sm" textAlign="center">
+                <Heading as="h4" size="md" color="gray.500" fontWeight="semibold" m={3}>
+                  Next Generation
+                </Heading>
+                <Text mb={5}>Created using HTML, CSS, Javascript and Bootstrap framework.</Text>
+              </Box>
             </Box>
-            <Box>
-              <Image src="../images/portfolio-3.png" alt="portfolio-3" />
+            <Box ml={5}>
+              <Link href="https://comptex.netlify.app" isExternal>
+                <Image src="../images/portfolio-3.png" alt="portfolio-3" />
+              </Link>
+              <Box color="gray.500" fontSize="sm" textAlign="center">
+                <Heading as="h4" size="md" color="gray.500" fontWeight="semibold" m={3}>
+                  Comptex Solutions
+                </Heading>
+                <Text mb={5}>Built with HTML, CSS, React JS and Chakra framework.</Text>
+              </Box>
             </Box>
-            <Box>
-              <Image src="../images/portfolio-4.png" alt="portfolio-4" />
+            <Box ml={5}>
+              <Link href="https://readaway.netlify.app" isExternal>
+                <Image src="../images/portfolio-4.png" alt="portfolio-4" />
+              </Link>
+              <Box color="gray.500" fontSize="sm" textAlign="center">
+                <Heading as="h4" size="md" color="gray.500" fontWeight="semibold" m={3}>
+                  Readaway
+                </Heading>
+                <Text mb={5}>Created using HTML, CSS, React JS and Bootstrap framework.</Text>
+              </Box>
             </Box>
           </SimpleGrid>
         </WhileInView>
