0ce0e532852f020caba79effc077965f1e94cb64
Mary A Green <cyberangel9911@gmail.com>
2022-04-24 23:18:13 -0500

    Update contact.ts

--- services/contact.ts
+++ services/contact.ts
@@ -1,12 +1,12 @@
-import { post } from '.'
+import axios from 'axios'
 
 // Contact us submission
 const sendContact = async (values = {} as any, encodedFormData: any) => {
   try {
     const headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
-    await post('/', encodedFormData, { headers })
+    const response = await axios.post('/', encodedFormData, { headers })
     const message = `Thank you ${values.name} for your form submission. I will be in touch with you shortly using the email, ${values.email}.`
-    return { success: true, message }
+    return { success: true, message, data: response.data }
   } catch (error) {
     throw new Error(error)
   }
